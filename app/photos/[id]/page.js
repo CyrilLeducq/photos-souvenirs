7596f83cbdaf6791e688bcd65b99e60b73de2cdf
CyrilLEDUCQ <cyril.leducq.pro@gmail.com>
2024-01-22 17:05:38 +0100

    Update  suppression erreurs

--- app/photos/[id]/page.js
+++ app/photos/[id]/page.js
@@ -9,12 +9,11 @@ async function getData(id) {
 }
 const Photo = async ({ params }) => {
     const data = await getData(params.id);
-    console.log(data);
     return (
         <div>
             <h1>{data.author}</h1>
-            <Image src={data.download_url} layout="responsive" width={data.width} height={data.height} />
-            <Link href={data.url}>Lien vers la source de l'image</Link>
+            <Image src={data.download_url} layout="responsive" width={data.width} height={data.height} alt="" />
+            <Link href={data.url}>Lien vers la source de l&apos;simage</Link>
         </div>
     )
 }
